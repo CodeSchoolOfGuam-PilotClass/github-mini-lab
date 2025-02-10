61057790126e39347bf9b3501f7c30379a4644dd
Leon Shimizu <lmshimizu@gmail.com>
2025-02-10 20:43:33 +1000

    Branching commands

--- test.rb
+++ test.rb
@@ -1,1 +1,4 @@
-puts "Hello Class"
+# git branch - shows the different branches available
+# git checkout -b [branch name] - creates a new branch with that branch name
+# git checkout [branch name] - switches to that branch
+
